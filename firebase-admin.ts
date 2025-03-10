a7eaa4a51806f82d49401170ae76a8532304e738
jostan30 <mathiasjostan@gmail.com>
2025-03-10 19:02:48 +0530

    Firestore adminDb config error solved

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,18 +1,29 @@
-import {App, initializeApp ,getApps ,getApp ,cert} from "firebase-admin/app";
+import { App, initializeApp, getApps, getApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("./service_key.json");
+const serviceKey = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
-let app:App;
+let app: App;
 
-if(getApps().length ===0){
-    app = initializeApp({
-        credential: cert(serviceKey),
-    });
-} else{
-    app = getApp();
+if (getApps().length === 0) {
+  app = initializeApp({
+    credential: cert(serviceKey as any),
+  });
+} else {
+  app = getApp();
 }
 
-const adminDb =getFirestore(app);
+const adminDb = getFirestore(app);
 
-export {app as adminApp , adminDb};
+export { app as adminApp, adminDb };
